9525f89e5cdbd0883db768d28117a6906b65e176
milyyy <milysong@163.com>
2020-12-19 12:44:12 +0800

    增加loading组件

---
+++ Loading/index.vue
@@ -0,0 +1,24 @@
+/**
+* author: milyyy
+* descriton: 只需父组件绑定loading即可
+*/
+<template>
+    <div
+        v-loading="loading"
+        element-loading-text="拼命加载中"
+        element-loading-spinner="el-icon-loading"
+        element-loading-background="rgba(0, 0, 0, 0.3)">
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        loading: {
+            type: Boolean,
+            default: false
+        }
+    }
+};
+</script>
